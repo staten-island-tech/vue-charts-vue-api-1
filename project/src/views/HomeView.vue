8227eea5d56bcace387f7f260aa285a993700815
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2024-03-05 10:21:13 -0500

    bar test

--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -1,7 +1,34 @@
-<script setup></script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
 </template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: ['January', 'February', 'March', 'April'],
+        datasets: [{ data: [40, 20, 12, 30] }]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
+</script>
