831339d1c7f692727c727a34024de07233387ccb
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2024-03-14 10:01:18 -0400

    1

---
+++ project/src/components/Squrrielcard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="squrrielcard">
+    <h2>{{ squrriel.unique_squirrel_id }}</h2>
+  </div>
+</template>
+
+<script>
+const props = defineProps({
+  squrriel: object
+})
+</script>
+
+<style lang="scss"></style>
--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -1,15 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <squrrielcard
+      v-for="squrriel in squrriels"
+      :key="squrriels.unique_squirrel_id"
+      :squrriel="squrriel"
+    ></squrrielcard>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-const squrriel = ref('')
+import { squrrielcard } from '../components/Squrrielcard.vue'
+const squrriels = ref('')
 async function getsqurriel() {
   let res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
   let data = await res.json()
-  squrriel.value = data
-  console.log(squrriel)
+  squrriels.value = data
+  console.log(squrriels)
 }
 onMounted(() => {
   getsqurriel()
