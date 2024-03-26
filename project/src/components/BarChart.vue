fbdd4d9dcae6285302231a02c2356da2ecc9f654
Samuelj58 <144460795+Samuelj58@users.noreply.github.com>
2024-03-25 23:09:50 -0400

    got the api to show on graph

---
+++ project/src/components/BarChart.vue
@@ -0,0 +1,50 @@
+<template>
+    <Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+    />
+  </template>
+  
+  <script>
+  import { Bar } from "vue-chartjs";
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, } from 'chart.js'
+  
+  
+  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+  
+  export default  {
+      name: 'BarChart',
+      components: { Bar }, 
+      props: {
+      data: Array,
+      labels: Array,
+    },
+      data(){
+          return{
+              chartData: {
+                  labels: this.labels,
+                  datasets: [ 
+                      {
+                        backgroundColor: '#f87979',
+                        label: "Number of squirrels with this fur color",
+                        data: this.data,
+                      }
+                  ]
+              },
+              chartOptions: {
+                  responsive: true
+              }
+          }
+      },
+  }
+  
+  </script>
+
+<style scoped>
+bar{
+max-width: 400px;
+max-height: 400px;
+}
+
+</style>
--- project/src/views/AboutView.vue
+++ project/src/views/AboutView.vue
@@ -1,85 +1,95 @@
 <template>
-  <div>
-    <canvas ref="chartCanvas"></canvas>
+  <div class="one">
+    <h1>Squirrel population vs. Fur color</h1>
+    <Chart v-if ="loaded" :data="Object.values(furstuff)" :labels="Object.keys(furstuff)"/>
+    <div class="two" @change="rechart">
+      <label>
+          Select a Filter
+          <select id="selection" v-model="agefilter">
+            <option value="nothing">No Filter</option>
+            <option v-for="filter in oldnessarray" :key="filter">{{ filter }}</option>
+          </select>
+        </label>
+    </div>
   </div>
 </template>
 
-<script>
-import Chart from 'chart.js/auto';
+<script setup>
+import { ref, onMounted, reactive  } from 'vue';
+import Chart from '@/components/BarChart.vue';
 
-export default {
-  mounted() {
-    this.fetchDataAndRenderChart();
-  },
-  methods: {
-    async fetchDataAndRenderChart() {
-      try {
-        const years = [2018, 2019, 2020];
+let oldnessarray = [];
+const loaded = ref(false);
+let data = reactive([]);
+const furstuff = ref(null);
+const agefilter = ref("nothing"); //default for dropdown select
 
-        const dataPromises = years.map(async year => {
-          const response = await fetch(`https://data.cityofnewyork.us/resource/vfnx-vebw.json?date=${year}`);
-          const data = await response.json();
-          return { year, data };
-        });
+onMounted(async() => {
+  const fetchAge = await fetch( //To get all the years
+    "https://data.cityofnewyork.us/resource/vfnx-vebw.json?$query=SELECT%20%60age%60%20GROUP%20BY%20%60age%60"
+  );
+  oldnessarray = await fetchAge.json();
+  oldnessarray = oldnessarray.map((age) => age.age);
+  console.log(oldnessarray);
+  rechart();
+});
 
-        const results = await Promise.all(dataPromises);
-
-        const datasets = results.map(result => {
-          const totalSightings = result.data.length;
-          return {
-            label: `Squirrel Sightings in ${result.year}`,
-            data: [totalSightings],
-            backgroundColor: 'pink',
-            borderColor: 'black',
-            borderWidth: 1
-          };
-        });
-
-        this.renderChart(datasets);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    },
-    renderChart(datasets) {
-      const ctx = this.$refs.chartCanvas.getContext('2d');
-      const chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: datasets.map(dataset => dataset.label),
-          datasets: datasets
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-              title: {
-                display: true,
-                text: 'Number of Sightings in Millions'
-              },
-            },
-            x: {
-              title: {
-                display: true,
-                text: 'Years',
-                padding: {
-                  top: 20, 
-                  bottom: 0,
-                  left: 0,
-                  right: 0
-                }
-              }
-            }
-          }
-        }
-      });
-    }
+async function rechart(){
+  loaded.value = false;
+  var props = Object.getOwnPropertyNames(furstuff);
+  for (var i = 0; i < props.length; i++) {
+    delete furstuff[props[i]];
   }
-};
+  for (var key in furstuff) {
+    delete furstuff[key];
+  } //clear out previous data
+
+  console.log(agefilter.value)
+  if (agefilter.value === "nothing") {
+    data = await fetchDataNormal();
+  } else {
+    data = await fetchChartData(agefilter.value);
+  }
+  console.log(data);
+  data.forEach((squirrel) => {
+    furstuff[squirrel.primary_fur_color] ??= 0;
+    furstuff[squirrel.primary_fur_color] += 1;
+  });
+  console.log(furstuff);
+  console.log(Object.keys(furstuff));
+  console.log(Object.values(furstuff));
+  loaded.value = true;
+}
+
+async function fetchChartData(age){
+  try {
+    let res = await fetch(`
+    https://data.cityofnewyork.us/resource/vfnx-vebw.json?$query=SELECT%0A%20%20%60x%60%2C%0A%20%20%60y%60%2C%0A%20%20%60unique_squirrel_id%60%2C%0A%20%20%60hectare%60%2C%0A%20%20%60shift%60%2C%0A%20%20%60date%60%2C%0A%20%20%60hectare_squirrel_number%60%2C%0A%20%20%60age%60%2C%0A%20%20%60primary_fur_color%60%2C%0A%20%20%60highlight_fur_color%60%2C%0A%20%20%60combination_of_primary_and%60%2C%0A%20%20%60color_notes%60%2C%0A%20%20%60location%60%2C%0A%20%20%60above_ground_sighter%60%2C%0A%20%20%60specific_location%60%2C%0A%20%20%60running%60%2C%0A%20%20%60chasing%60%2C%0A%20%20%60climbing%60%2C%0A%20%20%60eating%60%2C%0A%20%20%60foraging%60%2C%0A%20%20%60other_activities%60%2C%0A%20%20%60kuks%60%2C%0A%20%20%60quaas%60%2C%0A%20%20%60moans%60%2C%0A%20%20%60tail_flags%60%2C%0A%20%20%60tail_twitches%60%2C%0A%20%20%60approaches%60%2C%0A%20%20%60indifferent%60%2C%0A%20%20%60runs_from%60%2C%0A%20%20%60other_interactions%60%2C%0A%20%20%60geocoded_column%60%2C%0A%20%20%60%3A%40computed_region_efsh_h5xi%60%2C%0A%20%20%60%3A%40computed_region_f5dn_yrer%60%2C%0A%20%20%60%3A%40computed_region_yeji_bk3q%60%2C%0A%20%20%60%3A%40computed_region_92fq_4b7q%60%2C%0A%20%20%60%3A%40computed_region_sbqj_enih%60%0AWHERE%20caseless_one_of(%60age%60%2C%20%22${age}%22)`);
+    let data = await res.json();
+    return data
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function fetchDataNormal(){
+  try {
+    let res = await fetch(`
+    https://data.cityofnewyork.us/resource/vfnx-vebw.json?$query=SELECT%0A%20%20%60x%60%2C%0A%20%20%60y%60%2C%0A%20%20%60unique_squirrel_id%60%2C%0A%20%20%60hectare%60%2C%0A%20%20%60shift%60%2C%0A%20%20%60date%60%2C%0A%20%20%60hectare_squirrel_number%60%2C%0A%20%20%60age%60%2C%0A%20%20%60primary_fur_color%60%2C%0A%20%20%60highlight_fur_color%60%2C%0A%20%20%60combination_of_primary_and%60%2C%0A%20%20%60color_notes%60%2C%0A%20%20%60location%60%2C%0A%20%20%60above_ground_sighter%60%2C%0A%20%20%60specific_location%60%2C%0A%20%20%60running%60%2C%0A%20%20%60chasing%60%2C%0A%20%20%60climbing%60%2C%0A%20%20%60eating%60%2C%0A%20%20%60foraging%60%2C%0A%20%20%60other_activities%60%2C%0A%20%20%60kuks%60%2C%0A%20%20%60quaas%60%2C%0A%20%20%60moans%60%2C%0A%20%20%60tail_flags%60%2C%0A%20%20%60tail_twitches%60%2C%0A%20%20%60approaches%60%2C%0A%20%20%60indifferent%60%2C%0A%20%20%60runs_from%60%2C%0A%20%20%60other_interactions%60%2C%0A%20%20%60geocoded_column%60%2C%0A%20%20%60%3A%40computed_region_efsh_h5xi%60%2C%0A%20%20%60%3A%40computed_region_f5dn_yrer%60%2C%0A%20%20%60%3A%40computed_region_yeji_bk3q%60%2C%0A%20%20%60%3A%40computed_region_92fq_4b7q%60%2C%0A%20%20%60%3A%40computed_region_sbqj_enih%60`);
+    let data = await res.json();
+    return data
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 </script>
 
-<style>
-canvas {
-  width: 100%;
-  height: 400px; /* Adjust height as needed */
+<style scoped>
+.one{
+  max-width: 900px;
+  max-height: 900px;
 }
+
+
 </style>
